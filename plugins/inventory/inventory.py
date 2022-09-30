120b38dc21483f52b3ae92f7c5e6f59f17888165
Alex Gittings <agitting96@gmail.com>
2022-09-30 11:59:44 +0100

    add testing

--- plugins/inventory/inventory.py
+++ plugins/inventory/inventory.py
@@ -13,7 +13,6 @@ DOCUMENTATION = """
       - Get inventory hosts from IP Fabric
     extends_document_fragments:
       - constructed
-      - inventory_cache
     options:
       plugin:
         description: token that ensures this is a source file for the 'ipfabric' plugin.
@@ -59,53 +58,51 @@ EXAMPLES = """
 plugin: ipfabric.ansible.inventory
 """
 
-import traceback
-from ansible.plugins.inventory import BaseInventoryPlugin, Constructable, Cacheable
-from ansible.module_utils.basic import missing_required_lib
-from ansible.module_utils._text import to_text
 from ansible.errors import AnsibleError
+from ansible.module_utils._text import to_text
+from ansible.module_utils.basic import missing_required_lib
+from ansible.plugins.inventory import BaseInventoryPlugin, Constructable, Cacheable
+import traceback
 
 IPFABRIC_IMP_ERR = None
 try:
-  from ipfabric import IPFClient
-  HAS_IPFABRIC = True
+    from ipfabric import IPFClient
+    HAS_IPFABRIC = True
 except ImportError:
-  IPFABRIC_IMP_ERR = traceback.format_exc()
-  HAS_IPFABRIC = False
+    IPFABRIC_IMP_ERR = traceback.format_exc()
+    HAS_IPFABRIC = False
+
 
 class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
     NAME = "ipfabric.ansible.inventory"
 
     def fetch_devices(self, ipf):
-      self.devices_list = ipf.inventory.devices.all()
+        self.devices_list = ipf.inventory.devices.all()
 
     def main(self):
-      ipf = IPFClient(**self.get_option('provider'))
-      self.fetch_devices(ipf)
+        ipf = IPFClient(**self.get_option('provider'))
+        self.fetch_devices(ipf)
 
-      for device in self.devices_list:
-        hostname = device['hostname']
-        if device["loginIp"] and (device['devType'] != 'ap'):
-          new_vars = dict()
-          self.inventory.add_host(host=to_text(hostname))
-          for devicevar, deviceval in device.items():
-            self.inventory.set_variable(to_text(hostname), devicevar.lower(), deviceval)
-          new_vars[devicevar.lower()] = deviceval
+        for device in self.devices_list:
+            hostname = device['hostname']
+            if device["loginIp"] and (device['devType'] != 'ap'):
+                new_vars = dict()
+                self.inventory.add_host(host=to_text(hostname))
+                for devicevar, deviceval in device.items():
+                    self.inventory.set_variable(to_text(hostname), devicevar.lower(), deviceval)
+                    new_vars[devicevar.lower()] = deviceval
 
-          strict = self.get_option('strict')
-          self._add_host_to_composed_groups(self.get_option('groups'), new_vars, to_text(hostname), strict=strict)
-          self._add_host_to_keyed_groups(self.get_option("keyed_groups"), new_vars, to_text(hostname), strict=strict)
-
+                    strict = self.get_option('strict')
+                    self._add_host_to_composed_groups(self.get_option('groups'), new_vars, to_text(hostname), strict=strict)
+                    self._add_host_to_keyed_groups(self.get_option("keyed_groups"), new_vars, to_text(hostname), strict=strict)
 
     def parse(self, inventory, loader, path, cache=True):
-      super(InventoryModule, self).parse(inventory, loader, path)
+        super(InventoryModule, self).parse(inventory, loader, path)
 
-      if not HAS_IPFABRIC:
-        raise AnsibleError(f'The IP Fabric dynamic inventory plugin requires ipfabric. Error: {missing_required_lib("ipfabric")}')
-      self._read_config_data(path=path)
-      self.use_cache = cache
+        if not HAS_IPFABRIC:
+            raise AnsibleError(f'The IP Fabric dynamic inventory plugin requires ipfabric. Error: {missing_required_lib("ipfabric")}')
 
-      # IPFabric
-      print(self.get_option('provider'))
+        self._read_config_data(path=path)
+        self.use_cache = cache
 
-      self.main()
+        self.main()
