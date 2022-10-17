96a8970c33c387d4d278c00722d5afe1e0f36612
Alex Gittings <agitting96@gmail.com>
2022-10-17 17:17:39 +0100

    add future imports

--- plugins/module_utils/module.py
+++ plugins/module_utils/module.py
@@ -1,4 +1,5 @@
-from inspect import trace
+from __future__ import (absolute_import, division, print_function)
+
 from ansible.module_utils.basic import env_fallback
 import traceback
 from ansible.module_utils.basic import missing_required_lib
